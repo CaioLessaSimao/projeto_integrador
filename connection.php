9e785dd7ec8cf170a934222a579ec5ac0c6f422c
Gabriel Marinho Borges <20181TIIMI0120@cefetes.br>
2021-11-09 11:27:16 -0300

    conexao

---
+++ connection.php
@@ -0,0 +1,9 @@
+<php?
+	$host = "ec2-35-171-171-27.compute-1.amazonaws.com";
+	$dbname = "d5sv9emdjldoo8";
+	$user = "hvezuusbcngwia";
+	$psswd = "8b3d451222a483b655813fc475c09efb981038b1fbc5c08279031e6587101eb3";
+
+	$conn = $pg_connect($host, $dbname, $user, $psswd);
+
+?>
