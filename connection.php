c2d338db9562613ddcdecccbc9064506cda5eb62
Gabriel Marinho <gmarinho14@gmail.com>
2021-11-10 13:11:48 -0300

    a

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
   $host = "ec2-35-171-171-27.compute-1.amazonaws.com";
-  $user = "d5sv9emdjldoo8";
+  $user = "hvezuusbcngwia";
   $psswd = "8b3d451222a483b655813fc475c09efb981038b1fbc5c08279031e6587101eb3";
   $db_name = "d5sv9emdjldoo8";
   $connstring = "host=$host port=5432 dbname=$db_name user=$user password=$psswd";
