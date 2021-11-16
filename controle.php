31fbb0a9129b6286039f858bfa0047541889cb03
Gabriel Marinho <gmarinho14@gmail.com>
2021-11-16 11:17:26 -0300

    a

--- controle.php
+++ controle.php
@@ -1,4 +1,22 @@
 <?php
+
+    function insertDir($nome_dir, $email_dir, $cargo, $id_comite, $nome_comite){
+        $sql3 = "INSERT INTO diretor (nome, email, login, senha,cargo, fk_comite_id) values ('$nome_dir', '$email_dir', 'to_be_defined', 'to_be_defined', '$cargo', $id_comite);";
+        $result3 = pg_query($conn, $sql3);
+                    
+        $sql4 = "SELECT id FROM diretor ORDER BY id DESC limit 1";
+        $result4 = pg_query($conn, $sql4);
+                    
+        $dir = pg_fetch_array($result4);
+        $id_dir = $dir[0];
+                    
+        $login = $nome_comite.".dir.".$id_dir;
+                    
+        $senha = $nome_comite ."." .strval(rand(0, 9)) .strval(rand(0, 9)) .strval(rand(0, 9));
+
+        $sql5 = "UPDATE diretor SET login = '$login', senha = '$senha' WHERE id = $id_dir;";
+        $result5 = pg_query($conn, $sql5);
+    }
 	require_once  "connection.php";
 
     $oper = $_REQUEST['funcao'];
@@ -55,83 +73,20 @@
                 $cargo="";
                 if($i == 0){
                     $cargo = "geral";
-                    $sql3 = "INSERT INTO diretor (nome, email, login, senha,cargo, fk_comite_id) values ('$nome_dir', '$email_dir', 'to_be_defined', 'to_be_defined', '$cargo', $id_comite);";
-                    $result3 = pg_query($conn, $sql1);
-                    
-                    $sql4 = "SELECT id FROM diretor ORDER BY id DESC limit 1";
-                    $result4 = pg_query($conn, $sql4);
-                    
-                    $dir = pg_fetch_array($result4);
-                    $id_dir = $dir[0];
-                    
-                    $login = $nome_comite.".dir.".$id_dir;
-                    
-                    $senha = $nome_comite ."." .strval(rand(0, 9)) .strval(rand(0, 9)) .strval(rand(0, 9));
-
-                    $sql5 = "UPDATE diretor SET login = '$login', senha = '$senha' WHERE id = $id_dir;";
-                    $result5 = pg_query($conn, $sql5);
+                    insertDir($nome_dir, $email_dir, $cargo, $id_comite, $nome_comite);
 
                 }
 
                 else{
                     $cargo = "assistente";
-                    $sql3 = "INSERT INTO diretor (nome, email, login, senha,cargo, fk_comite_id) values ('$nome_dir', '$email_dir', 'to_be_defined', 'to_be_defined', '$cargo', $id_comite);";
-                    $result3 = pg_query($conn, $sql3);
+                    insertDir($nome_dir, $email_dir, $cargo, $id_comite, $nome_comite);
                     
-                    $sql4 = "SELECT id FROM diretor ORDER BY id DESC limit 1";
-                    $result4 = pg_query($conn, $sql4);
-                    
-                    $dir = pg_fetch_array($result4);
-                    $id_dir = $dir[0];
-                    
-                    $login = $nome_comite.".dir.".$id_dir;
-                    
-                    $senha = $nome_comite ."." .strval(rand(0, 9)) .strval(rand(0, 9)) .strval(rand(0, 9));
-
-                    $sql5 = "UPDATE diretor SET login = '$login', senha = '$senha' WHERE id = $id_dir;";
-                    $result5 = pg_query($conn, $sql5);
                 } 
             }
             echo  "<script>alert('Cadastro realizado com sucesso!');</script>";
-            header("Location: add_delegacao.html");
+            header("Location: add_delegacao.php");
         }  
     } 
-    /**else if ($oper == "inserir_diretor") {
-        $nome = "";
-        $email = "";
-        $login = "to_be_defined";
-        $senha = "to_be_defined";
-        $cargo = "geral";
-        $id_comite = $_REQUEST["id_comite"];
-        if(isset($_REQUEST["nome"]) && isset($_REQUEST["email"])){
-            $nome = $_REQUEST["nome"];
-            $email = $_REQUEST["email"];
-            
-            $sql1 = "INSERT INTO diretor (nome, email, login, senha,cargo, fk_comite_id) values ('$nome', '$email', '$login', '$senha', '$cargo', '$id_comite');";
-            $result1 = pg_query($conn, $sql1);
 
-            $sql2 = "SELECT id FROM diretor ORDER BY id DESC LIMIT 1;";
-            $result2 = pg_query($conn, $sql2);
-            $diretor = pg_fetch_array($result2);
-            $id_dir = strval($diretor[0]);
-
-            $sql3 = "SELECT nome FROM comite WHERE id = '$id_comite';";
-            $result3 = pg_query($conn, $sql3);
-            $comite = pg_fetch_array($result3);
-
-            $nome_comite = $comite[0]; 
-
-            $login = $nome_comite.".dir.".$id_dir;
-
-            $senha = $nome "." .strval(rand(0, 9)) .strval(rand(0, 9)) .strval(rand(0, 9));
-
-            $sql4 = "UPDATE TABLE SET login = '$login', senha = '$senha' WHERE id = $id_dir;";
-            $result4 = pg_query($conn, $sql4);
-
-            echo "sucesso";
-            
-        }
-
-    }**/
 
 ?>
