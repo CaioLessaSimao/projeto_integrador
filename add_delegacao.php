18972fbeebad3ef03d15213893799cf4b7a60a1b
MountSome <caiolessa02@gmail.com>
2021-11-17 10:54:11 -0300

    ifj

--- add_delegacao.php
+++ add_delegacao.php
@@ -134,5 +134,7 @@
             </div>
 
 	</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
+    <?php echo "funcionou"; ?>
+</script>
 </html>
