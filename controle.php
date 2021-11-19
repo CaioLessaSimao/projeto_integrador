803af2e5354070425d989d9a374d6c907bf1a2d4
MountSome <caiolessa02@gmail.com>
2021-11-19 11:32:32 -0300

    asdasd

--- controle.php
+++ controle.php
@@ -141,7 +141,24 @@
     }
 
     if($oper == "logar_dir"){
-        echo "teste";
+
+        if(isset($_REQUEST['dir_usuario']) && isset($_REQUEST['dir_senha'])){
+            $login = $_REQUEST['dir_usuario'];
+            
+            $senha = $_REQUEST['dir_senha'];
+            
+            $sql8 = "SELECT login,senha FROM diretor"; 
+
+            $result8 = pg_query($conn,$sql8);
+
+            $array = pg_fetch_array($result8);
+
+            echo $array[0];
+        }
+
+
+
+        
     }
 
 ?>
