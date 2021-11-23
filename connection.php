57e2dd3d7dacefef3af9e985276d1cf17ad14d06
Caio Lessa Simao <20181TIIMI0049@cefetes.br>
2021-11-23 11:45:37 -0300

    sdafj

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
   $user = "hvezuusbcngwia";
   $psswd = "8b3d451222a483b655813fc475c09efb981038b1fbc5c08279031e6587101eb3";
   $db_name = "d5sv9emdjldoo8";
-  $connstring = "host=$host port=5432 dbname=$db_name user=$user password=$psswd";
+  $connstring = "host=$host port=5432 dbname=$db_name user=$user password=$psswd sslmode=required";
 
   $conn = pg_connect($connstring) or die("Falha na conexão");
 ?>
