b9a55ed9644f32141bd0aedba87d4ba15c7359a9
MountSome <caiolessa02@gmail.com>
2021-11-25 13:05:34 -0300

    ad

--- controle.php
+++ controle.php
@@ -187,7 +187,7 @@
 
                     //echo count($tst);
 
-                    for($i=0;$i<count($tst);$i+=1){
+                    for($i=0;$i<count($tst)-1;$i+=1){
                         echo $tst[$i]."<br>";
                     }
                     /*
