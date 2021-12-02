4a373e223ee354055c942a91b518dc9527c2cee4
MountSome <caiolessa02@gmail.com>
2021-12-02 10:55:15 -0300

    sdias

---
+++ pag_comite.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Página do comitê</title>
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+	<nav>
+    	<div class="nav-wrapper green accent-3">
+      		
+      		<a href="index.php" class="brand-logo">PSGD</a>
+      		
+      		<ul id="nav-mobile" class="right hide-on-med-and-down">
+        		<li><a href="#"><i class="material-icons right">account_circle</i></a></li>
+      		</ul>
+    	
+    	</div>
+  	</nav>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
