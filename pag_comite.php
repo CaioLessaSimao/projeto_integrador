ad6ba03829189b6ae12b1cf569492997c8e12ffa
MountSome <caiolessa02@gmail.com>
2021-12-02 14:55:54 -0300

    sdias

--- pag_comite.php
+++ pag_comite.php
@@ -8,12 +8,21 @@
 	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<style type="text/css">
-		#img_principal{
-			background: url(teste2.jpg) no-repeat center center fixed; 
-  			-webkit-background-size: cover;
-  			-moz-background-size: cover;
-  			-o-background-size: cover;
-  			background-size: cover; 
+		div{
+		    width: 100%;
+		    height: 560px;
+		}
+		.minhaClass{
+		    background: url(teste2.jpg) no-repeat center center;
+		    -webkit-background-size: cover;
+		    -moz-background-size: cover;
+		    -o-background-size: cover;
+		    background-size: cover;
+		    z-index: 1;
+		}
+		.quadro{
+		  z-index: 2;
+		  background-color: red;
 		}
 	</style>
 </head>
@@ -29,13 +38,20 @@
     	
     	</div>
   	</nav>
-	 
 
-	    <div class="row" id="img_principal">
-	    	<!--<img class="responsive-img" src="teste2.jpg">-->
-	    </div>
+	<div class="minhaClass">
+  		<div class="quadro">
+    		<h2>Lista de Delegações</h2>
+  		</div>
+	</div>
 
-  	
+	<div class="minhaClass">
+  		<h2>Lista de DPOs</h2>
+	</div>
+	
+	<div class="minhaClass">
+  		<h2>Iniciar Simulação</h2>
+	</div>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
