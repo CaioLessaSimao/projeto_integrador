f600cd378fc326f5506c6a129fd021fd53b73a76
MountSome <caiolessa02@gmail.com>
2021-12-06 12:13:27 -0300

    sdias

--- lst_del.php
+++ lst_del.php
@@ -1,28 +1,51 @@
 <html>
 <head>
 	<title>Lista de delegados</title>
+	<?php
+		require_once "connection.php";
+		
+		$comite = (int)$_REQUEST['idcomite'];
+		$sql = "SELECT nome, email FROM delegacao WHERE fk_comite_id=$comite;";
+		
+		$result = pg_query($conn,$sql);
+		$array = pg_fetch_array($result);
+		
+		$tbdelegacoes = [];
+		
+		for($i=0; $i < count($array)-1; $i+=2){
+			$nome = $array[$i];
+			
+			$email = $array[$i+1];
+			
+			$linha = new create_item($nome,$email);
+			
+			$tbdelegacoes[] = $linha;
+		}
+		$resultado = "";
+		foreach($tbdelegacoes as $i){
+			$resultado .= $i->html;
+		}
+
+		function create_item($nome,$email){
+			$this->nome = $nome;
+			$this->email = $email;
+			$string = "<tr><td>".$nome."</td><td>".$email."</td><td></tr";
+			$this->html = $string;
+		}
+
+	?>
 </head>
 <body>
 	<table>
-		<?php 
-			require_once "connection.php";
-			
-			if(isset($_REQUEST['idcomite'])){
-				$comite = (int)$_REQUEST['idcomite'];
-				$sql = "SELECT nome,email FROM delegacao WHERE fk_comite_id=$comite";
-				$result = pg_query($conn,$sql);
-				$aux = pg_fetch_array($result);
-				echo count($aux)."<br>";
-				while ($dados = pg_fetch_array($result)):
-		?>
-		<tr>
-			<td><?php echo $dados['nome']; ?></td>
-			<td><?php echo $dados['dpo'];?></td>
-		</tr>
-		<?php 
-			endwhile;
-			}
-		?>
+		<thead>
+			<tr>
+				<th>Nome da Delegação</th>
+				<th>Email do delegado</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php echo $resultado; ?>
+		</tbody>
 	</table>
 </body>
 </html>
