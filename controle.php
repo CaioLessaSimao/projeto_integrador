63e1e794853efaa007f6d9402512e5d9c9c750d8
Gabriel Marinho <gmarinho14@gmail.com>
2021-12-16 11:30:16 -0300

    del_del

--- controle.php
+++ controle.php
@@ -216,12 +216,12 @@
         $id_comite = $_REQUEST['idcomite'];
 
         $sql = "DELETE FROM delegacao WHERE id = $id;";
-        $result = pg_query($con, $sql);
+        $result = pg_query($conn, $sql);
 
         header("Location: lst_del.php?idcomite=$id_comite");
 
     }
-        
+    echo $oper;
         
 
 ?>
