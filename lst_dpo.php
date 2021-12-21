d8eb08430a62c410c400d175533852b31f0ee8c8
Weverton <wevertoncapixaba22@gmail.com>
2021-12-21 18:41:30 -0300

    upload

--- lst_dpo.php
+++ lst_dpo.php
@@ -3,25 +3,34 @@
 	<title>Lista de DPOs</title>
 </head>
 <body>
+
+
+
+	
+<form action="" method="POST">
+	
+	<input type="file" id="dpoid" name="dpo" accept="application/pdf">
+	<label for="dpo">Adicionar DPO: </label>
+
+	<input type="hidden" name="funcao" value="upload">
+	
+	<input type="submit" name="submit" value="Alterar">
+
+</form>
+	
+
 	<table>
-		<?php 
-			require_once "connection.php";
-			
-			if(isset($_REQUEST['idcomite'])){
-				$comite = (int)$_REQUEST['idcomite'];
-				$sql = "SELECT nome,dpo FROM delegacao WHERE fk_comite_id=$comite";
-				$result = pg_query($conn,$sql);
-				while ($dados = pg_fetch_array($result)):
-			
-		?>
+	
 		<tr>
-			<td><?php echo $dados['nome']; ?></td>
-			<td><?php echo $dados['dpo'];?></td>
+
+			<td></td>
+			<td></td>
+		
 		</tr>
-		<?php 
-			endwhile;
-			}
-		?>
+
+
+		
 	</table>
+
 </body>
 </html>
