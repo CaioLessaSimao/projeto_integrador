be68692207a4392aab8bc3e872047a7733856816
MountSome <caiolessa02@gmail.com>
2021-12-23 13:18:40 -0300

    wes anderson

--- comiteMobile.php
+++ comiteMobile.php
@@ -38,7 +38,7 @@ if($isAuth) {
 
 		$idComite = (int)$_GET['idComite'];
 
-		$sql = "SELECT nome,tema FROM delegacao WHERE fk_comite_id=$idComite;";
+		$sql = "SELECT nome,tema FROM comite WHERE id=$idComite;";
 
 		$query = pg_query($con, $sql);
 
