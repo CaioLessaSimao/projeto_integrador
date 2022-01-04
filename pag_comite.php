a7c95c7a4177254c4b582b0fc36b76c967998768
MountSome <caiolessa02@gmail.com>
2022-01-04 19:44:38 -0300

    spilberg

--- pag_comite.php
+++ pag_comite.php
@@ -79,6 +79,7 @@
 					<div class="col s12">
 						<button type="button" class="btn" onclick="window.location.href='lst_del.php?idcomite=<?php echo $comite2; ?>'">Lista de delegações</button>
 						<button type="button" class="btn" onclick="">Lista de DPOs</button>
+						<button type="button" class="btn" onclick="">Iniciar simulação</button>
 					</div>		
 				</div>
 				
