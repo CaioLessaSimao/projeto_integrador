2055727045893ebac3eabf24b5c8b5a925ab59b8
MountSome <caiolessa02@gmail.com>
2022-01-03 22:09:22 -0300

    jose padilla

--- controle.php
+++ controle.php
@@ -153,8 +153,6 @@
             $mensagem = "";
             $emailDirG = "";
 
-            echo count($arrayDir);
-
 
             for($i == 0; $i < count($arrayDir); $i++){
                 $array = $arrayDir[$i];
@@ -190,7 +188,7 @@
 
 
 
-            //var_dump($mensagem);
+            var_dump($mensagem);
 
             mail($emailDirG, "Logins do comite ".$nomeComite, $mensagem);
 
