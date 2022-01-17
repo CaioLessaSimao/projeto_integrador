e429a829591422d3685cea0e16b5c7b5bd99196b
MountSome <caiolessa02@gmail.com>
2022-01-17 10:27:38 -0300

    ads

--- simu_dir.php
+++ simu_dir.php
@@ -38,6 +38,8 @@
 	</div>
 </body>
 <script type="text/javascript">
+	var instance = M.FloatingActionButton.getInstance(elem);
+
 	document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
