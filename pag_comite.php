0b53e75bb2d464d1ccf5a64ab81243f63d58e48a
MountSome <caiolessa02@gmail.com>
2022-01-17 10:15:37 -0300

    ads

--- pag_comite.php
+++ pag_comite.php
@@ -78,8 +78,9 @@
 				<div class="row">
 					<div class="col s12">
 						<button type="button" class="btn" onclick="window.location.href='lst_del.php?idcomite=<?php echo $comite2; ?>'">Lista de delegações</button>
+						<button type="button" class="btn" onclick="window.location.href='simu_dir.php?idcomite=<?php echo $comite2; ?>'">Iniciar simulação</button>
 						<button type="button" class="btn" onclick="">Lista de DPOs</button>
-						<button type="button" class="btn" onclick="">Iniciar simulação</button>
+						
 					</div>		
 				</div>
 				
