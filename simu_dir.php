cc9be506e275a3157ecc4acc2bf7b8ea4dbbe6c3
MountSome <caiolessa02@gmail.com>
2022-01-17 10:33:25 -0300

    ads

--- simu_dir.php
+++ simu_dir.php
@@ -38,9 +38,8 @@
 	</div>
 </body>
 <script type="text/javascript">
-	document.addEventListener('DOMContentLoaded', function() {
+	
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, options);
-  });
 </script>
 </html>
