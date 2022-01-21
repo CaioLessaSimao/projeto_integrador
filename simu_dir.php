0b0f85001b02d78b8233ace57a55b4ccc52f14d1
MountSome <caiolessa02@gmail.com>
2022-01-21 17:09:22 -0300

    adiajd

--- simu_dir.php
+++ simu_dir.php
@@ -156,9 +156,29 @@
 
 	function inicia(){
 		interval = setInterval("conta();",1000);
-		let ajax = new XMLHttpRequest();
-		ajax.open("post", "controle.php");
-	}
+		var data = {status: "iniciar"};
+	    
+	    let ajax = new XMLHttpRequest();
+
+            ajax.open('post', 'msg.php');
+
+            ajax.onreadystatechange = function(){
+                if (
+                    ajax.readyState == 4
+                    && ajax.status >= 200
+                    && ajax.status <= 400
+                ) {
+                    let respostaAjax = JSON.parse(ajax.responseText);
+
+                    // Aqui os dados já foram tratados.
+                    // Faça o que quiser com eles:
+                    console.log(respostaAjax);
+                }
+            }
+            var aux = JSON.stringify(data);
+            console.log(aux);
+            ajax.send(aux);
+        }
 
 	function para(){
 		clearInterval(interval);
