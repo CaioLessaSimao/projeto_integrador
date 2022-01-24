c7e64af655595ee28506793a71ed7ff5154106fa
Caio Lessa Simao <20181TIIMI0049@cefetes.br>
2022-01-24 18:00:08 -0300

    saushuas

--- simu_dir.php
+++ simu_dir.php
@@ -78,11 +78,11 @@
 
     <div class="col s6" id="lst">
 		<p>Lista de oradores</p>
-		<div class="fixed-action-btn">
-	  		<a data-target="meu-modal" class="btn-floating btn-large red modal-trigger" onclick="sel_del()">
-	    		<i class="large material-icons">mode_edit</i>
-	  		</a>
-		</div>
+	</div>
+	<div class="fixed-action-btn">
+	  	<a data-target="meu-modal" class="btn-floating btn-large red modal-trigger" onclick="sel_del()">
+	    	<i class="large material-icons">mode_edit</i>
+	  	</a>
 	</div>
 
 	<div class="container">
